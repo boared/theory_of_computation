1afdf7cd987a339fe3089c5bd125eca83f42d144
Eder <ederp@amazon.com>
2018-07-20 20:03:12 +0200

    Changing TransitionFunction to support sets as range values.

--- finite_automata/TransitionFunction.h
+++ finite_automata/TransitionFunction.h
@@ -2,22 +2,28 @@
 #define TRANSITION_FUNCTION_H
 
 #include <map>
+#include <set>
 #include "State.h"
 
 /**
- * Maps a pair <State, symbol> to a State
+ * Maps a pair<State, symbol> to a set<State>
  */
 class TransitionFunction {
     private:
-        std::map<std::pair<State, std::string>, State> mapping;
+        std::map<std::pair<State, std::string>, std::set<State>> _mapping;
 
     public:
         void addTransition(const State& stateDomain, const std::string& symbolDomain, const State& stateRange) {
-            mapping[std::make_pair(stateDomain, symbolDomain)] = stateRange;
+            _mapping[std::make_pair(stateDomain, symbolDomain)].insert(stateRange);
         }
 
-        State compute(const State& state, const std::string& symbol) {
-            return mapping[std::make_pair(state, symbol)];
+        std::set<State> mapping(const State& state, const std::string& symbol) {
+            const auto& x = std::make_pair(state, symbol);
+            if (_mapping.find(x) != _mapping.end()) {
+                return _mapping[x];
+            } else {
+                return std::set<State>();
+            }
         }
 };
 #endif
